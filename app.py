1eb3a25253a579b5ca996aa07b3c7525c45e57f2
Raj Aryan Dash <rajaryanrad@gmail.com>
2022-11-21 22:13:59 +0530

    Add in logic for calculation and display
    * Add in logic in app.py to pull and push values and calculate
    * Add the following in index.html:
        * text box for operation type
        * default values for all text boxes
        * add logic to display answer

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=['POST', 'GET'])
 def calculate():
-    return render_template('index.html')
+    ans=''
+    if request.method=='POST':
+        try:
+            a = float(request.form.get('a'))
+            b = float(request.form.get('b'))
+            operation = request.form.get('op').lower()
+            if operation == 'add':
+                ans = a+b
+            elif operation == 'subtract':
+                ans = a-b
+            elif operation == 'multiply':
+                ans = a*b
+            elif operation == 'divide':
+                ans = a/b
+            else:
+                ans = "Enter valid values"
+        except ValueError:
+            ans = "Enter Valid Values"
+    return render_template('index.html', ansh=ans)
 
 if __name__ == "__main__":
     app.run(debug=True)
