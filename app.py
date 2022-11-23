c8c24e250df015ec3e5cbd9244849a16b81f0de0
Raj Aryan Dash <rajaryanrad@gmail.com>
2022-11-23 18:20:55 +0530

    Setup everything for advanced calculator

--- app.py
+++ app.py
@@ -29,5 +29,16 @@ def calculate():
         color = "alert-danger"
     return render_template('simple.html', result=result, note=note, color=color)
 
+@app.route('/advanced')
+def advanced():
+    return render_template('advanced.html')
+
+@app.route('/calculate_advanced', methods=['post'])
+def calculate_advanced():
+    result = note = color = ''
+    first = float(request.form['firstNumber'])
+    operation = request.form['operation']
+    return render_template('advanced.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
